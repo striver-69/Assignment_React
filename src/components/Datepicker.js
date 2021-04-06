dc0d3a8239fcaf17249f57f51fd8c79bf0280fb9
Chiranjivee <chiranjivee960@gmail.com>
2021-04-07 00:28:21 +0530

    updated search field

--- src/components/Datepicker.js
+++ src/components/Datepicker.js
@@ -4,7 +4,7 @@ function Datepicker({startDate,setStartDate}) {
   return (
     <div className="ui centered grid">
       <div class="ui icon input">
-        <input type="number" placeholder="Search..." value={startDate} onChange={(e)=>setStartDate(e.target.value)}/>
+        <input type="number" placeholder="Set launching Year..." value={startDate} onChange={(e)=>setStartDate(e.target.value)}/>
       </div>
     </div>
   )
